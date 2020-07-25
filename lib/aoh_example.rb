33bb970aae53bda499122e6bda435895f5daee64
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-25 19:29:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,9 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  literal_array = [
+    {name:"Don Gately", occupation: "Live-in Staff"}
+    {name:Joelle van Dyne", occupation: "Rad}]
 end
 
 def aoh_lookup(aoh, row, key)
